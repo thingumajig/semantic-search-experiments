35d1c80e29d507bf39e7da049b6a6d05bcb8eb72
Roman Kozachek <roman_you_are@criptext.com>
2022-07-13 15:06:19 +0300

    Fixed small bug in embeddings search

--- streamlit_docker/pages/Search by emb.py
+++ streamlit_docker/pages/Search by emb.py
@@ -25,7 +25,7 @@ column_name = st.text_input("Введите column_name:")
 title = st.text_input("Введите текст запроса:")
 num_results = json.load(open("./settings.json", "r"))["search_k"]
 if st.button("Поиск"):
-    result = requests.post(f"http://172.21.0.4:80/semantic_search/", data={"index":index, "column_name":column_name, "search_text": title, "num_results": num_results}).json()
+    result = requests.post(f"http://172.21.0.4:80/semantic_search/", data={"index":index, "column_name":column_name, "query": title, "num_results": num_results}).json()
     st.write(f"Было найдено {result['hits']['total']['value']} совпадений. Показаны топ-{num_results}")
     st.dataframe(create_df_from_json(result, num_results), width=1000)
     
